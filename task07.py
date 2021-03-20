53c7a8c2feb5fdcc46e945a347ad1df1d33af151
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-20 16:39:08 +0200

    Update task07.py

--- task07.py
+++ task07.py
@@ -1,4 +1,7 @@
-celsius = float(input())
 def fahrenheit_func(c):
      return (c * (9 / 5)) + 32
-print(fahrenheit_func(celsius))
+print(fahrenheit_func(36))
+
+def celsius_func(f):
+     return (f - 32)*(5 / 9)
+print(celsius_func(36))
