4251e31d69d6d581314b9e484f0498c02b76efa9
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-20 16:38:12 +0200

    Update task06.py

--- task06.py
+++ task06.py
@@ -1,14 +1,12 @@
-num1 = int(input(5))
-num2 = int(input(9))
-num3 = int(input(4))
+def Max_num(num1, num2,num3):
+    if (num1 >= num2):
+        if(num1 >= num3):
+           return num1
+        else:
+            return num3
+    elif  (num2 >= num3):
+        return num2
 
-if (num1 >= num2):
-    if(num1 >= num3):
-       print( " num1 is the largest")
     else:
-        print("num3 is the largest")
-elif  (num2 >= num3):
-    print("num2 is the largest")
-
-else:
-    print("num3 is the largest")
+        return num3
+print(Max_num(5, 9, 4))
