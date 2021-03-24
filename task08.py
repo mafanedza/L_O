76ec76db8bc6353945c806596e0a513c2cc74fdf
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-24 14:13:33 +0200

    Update task08.py

--- task08.py
+++ task08.py
@@ -1,6 +1,10 @@
 def hours(c):
-    if c//60 < 2:
+    if c//60 < 2 and c%60 >=2:
       return str(c//60) + " hour, " + str(c%60) + " minutes"
-    else:
+    elif c%60 >= 2 and c//60 >=2:
         return str(c//60) + " hours, " + str(c%60) + " minutes"
-print(hours(126))
+    elif c//60 < 2 and c%60 < 2:
+        return str(c//60) + " hour, " + str(c%60) + " minute"
+    elif c//60 >= 2 and c%60 < 2:
+        return str(c//60) + " hours, " + str(c%60) + " minute"
+print(hours(133))
