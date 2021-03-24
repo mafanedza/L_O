2c7a7512a3ae107dcd1b0e78f823547b023a325c
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-24 17:05:36 +0200

    Update task07.py

--- task07.py
+++ task07.py
@@ -1,7 +1,7 @@
-def fahrenheit_func(c):
-     return (c * (9 / 5)) + 32
+def fahrenheit_func(celsius):
+     return (celsius * (9 / 5)) + 32
 print(fahrenheit_func(36))
 
-def celsius_func(f):
-     return (f - 32)*(5 / 9)
+def celsius_func(fahrenheit):
+     return (fahrenheit - 32)*(5 / 9)
 print(celsius_func(36))
