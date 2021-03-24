e114c88cb27c3660d161d675be8401625f14eb23
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-24 17:02:57 +0200

    Update task05.py

--- task05.py
+++ task05.py
@@ -1,5 +1,5 @@
-def triangle_area(a,b,c):
-    s = (a + b + c) / 2
-    area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+def triangle_area(side1,side2,side3):
+    semi_parameter = (side1 + side2 + side3) / 2
+    area = (semi_parameter*(semi_parameter-side1)*(semi_parameter-side2)*(semi_parameter-side3)) ** 0.5
     return area
 print(triangle_area(12,16,20))
