d73deb6dc44f73057f58f6f3f6bb06988c04c771
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-24 16:32:29 +0200

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,7 +1,8 @@
 word_1 = "cans"
 word_2 = "cons"
 def common_letters(x,y):
-    for i in x :
-        if i in y:
-            print("common letter " + i)
-print(common_letters(word_1,word_2))
+    s1 = set(x)
+    s2 = set(y)
+    lst = (s1 & s2)
+    print('common letters:' + str(lst))
+common_letters(word_1,word_2)
