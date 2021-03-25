363842a78ee0e2d300f54b537d80ceb2671fbec1
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-26 00:43:36 +0200

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,9 +1,9 @@
 word_1 = "cans"
 word_2 = "cons"
-def common_letters(x,y):
-    s1 = set(x)
-    s2 = set(y)
-    lst = (s1 & s2)
+def common_letters(word1,word2):
+    set1 = set(word1)
+    set2 = set(word2)
+    lst = (set1 & set2)
     lst_as_str = ','.join(lst)
     print('common letters:' + lst_as_str)
-common_letters(word_1,word_2)
+common_letters(word_1,word_2);
