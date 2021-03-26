845bfd47f9e2e690de35159301a21a26269cd0df
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-26 12:11:50 +0200

    Update task10.py

--- task10.py
+++ task10.py
@@ -5,5 +5,5 @@ def common_letters(word1,word2):
     set2 = set(word2)
     lst = (set1 & set2)
     lst_as_str = ','.join(lst)
-    print('common letters:' + lst_as_str)
-common_letters(word_1,word_2);
+    return 'common letters:' + lst_as_str
+print(common_letters(word_1,word_2));
