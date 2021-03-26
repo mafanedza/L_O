cd6ad5099b4704daeb188878ba948d9d84775a67
mafanedza <75442703+mafanedza@users.noreply.github.com>
2021-03-26 12:10:05 +0200

    Update task09.py

--- task09.py
+++ task09.py
@@ -4,5 +4,5 @@ def vowel(word):
     vowels_as_set = set(vowels)
     lst = (word_as_set & vowels_as_set )
     lst_as_str = ', '.join(lst)
-    print("Vowels: "+lst_as_str)
-vowel('Mafanedza');
+    return "Vowels: "+ lst_as_str
+print(vowel('Mafanedza'));
